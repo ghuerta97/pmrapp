876c8095759575b7d18dea2715e9c0b5303afe5b
Gustavo Huerta <g.huerta01@ufromail.cl>
2020-03-25 16:15:09 +0100

    update url

--- Fronted/src/environments/environment.ts
+++ Fronted/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:8080/'
-}
+  api: 'https://pmrappteam.herokuapp.com/'
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
